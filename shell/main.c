548a3521755709c5335a959908dbb5556963d77b
Mubeen-Ul-Hassan <mubeenulhassan396@gmail.com>
2025-03-07 09:05:47 +0500

    exevp() and fork() system call added.

--- shell/main.c
+++ shell/main.c
@@ -2,36 +2,48 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h> // exit()
 
-int main() {
-
+int main()
+{
     char string[20];
+
     printf("Enter the string: ");
     fgets(string, 20, stdin);
     printf("\nThe string is: %s", string);
 
-    char * token;
-
+    char *token;
     char delimiter[] = " ";
-    
+
     token = strtok(string, delimiter);
 
-    while(token){
+    while (token)
+    {
         printf("%s\n", token);
-        token = strtok(NULL, delimiter); // "Null" tells strtok() to resume tokenizing from the previously saved the position. 
-
+        token = strtok(NULL, delimiter); // "Null" to resume tokenizing from the previously saved the position.
     }
+
+    // Fork start
+    int pid;
+    pid = fork();
+
+    if (pid == 0)
+    {
+        printf("I am the child process\n");
+    }
+
+    printf("Process ID: %d\n", getpid());
+    // Fork end
 
     return 0;
 }
-
 
 /*
         strtok(arg1, arg2)
         arg1 - original string to be passed.
         arg2 - seprator using which we tokenize the string.
 */
-
 
 /*
         fgets(argument1, argument2, argument3);
